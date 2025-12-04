4e85d0e0d295f5b51d54c416176f7e2b1784eb20
chizihn <chizihn@gmail.com>
2025-12-04 18:18:10 +0100

    fix build error 2

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -834,8 +834,8 @@ const DashboardPage: React.FC = () => {
                       cx="50%"
                       cy="50%"
                       labelLine={false}
-                      label={({ state, percent = 0 }) =>
-                        `${state} ${(percent * 100).toFixed(0)}%`
+                      label={({ payload, percent = 0 }) =>
+                        `${payload.state} ${(percent * 100).toFixed(0)}%`
                       }
                       outerRadius={80}
                       fill="#8884d8"
